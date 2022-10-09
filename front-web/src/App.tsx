638606f14e0566e1c5bcb380bc873f15e6cb5531
Jonas Souza <jonasmzsouza@gmail.com>
2022-10-09 16:34:12 -0300

    update(front-web): add piechartcard component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Filter from './components/filter';
 import Header from './components/header';
+import PieChartCard from './components/pie-chart-card';
 import SalesByDateComponent from './components/sales-by-date';
 import SalesSummaryComponent from './components/sales-summary';
 
@@ -13,6 +14,16 @@ function App() {
         <SalesByDateComponent />
         <div className="sales-overview-container">
           <SalesSummaryComponent />
+          <PieChartCard
+            name={'Lojas'}
+            labels={['Uberlândia', 'Araguari', 'Uberaba']}
+            series={[30, 40, 30]}
+          />
+          <PieChartCard
+            name={'Pagamento'}
+            labels={['Crédito', 'Débito', 'Dinheiro']}
+            series={[50, 30, 20]}
+          />
         </div>
       </div>
     </>
